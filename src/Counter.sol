1ac8744e651cf35e55db6cba8615a180e908c14f
Calnix <calnix.289@gmail.com>
2024-05-12 12:48:08 +0800

    1st draft

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {Pausable} from "openzeppelin-contracts/contracts/utils/Pausable.sol";
+import {Ownable2Step, Ownable} from  "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+
+/**
+ note:
+  - is there an end time?
+
+ */
 contract SimpleStaking {
+    using SafeERC20 for IERC20;
 
     IERC20 public MOCA_TOKEN;
 
@@ -13,19 +25,97 @@ contract SimpleStaking {
 
     mapping(address user => mapping (bytes32 id => Data userData)) public users;
     //mapping(bytes32 id => uint256 balance) public balances;
-    mapping(bytes32 id => Data idData) public balances;
+    mapping(bytes32 id => Data idData) public ids;
 
-    constructor(){}
+    
+    event Staked(address indexed user, bytes32 indexed id, uint256 amount);
+    event Unstaked(address indexed user, bytes32 indexed id, uint256 amount);
+
+
+    constructor(address mocaToken){
+        
+        MOCA_TOKEN = IERC20(mocaToken);
+    }
 
     function stake(bytes32 id, uint256 amount) external {
         require(id != bytes32(0), "Invalid id");
+        
+        // check if id exists
+        // note: how?
 
+        // cache
+        Data memory idData = ids[msg.sender];
+        Data memory userData = users[msg.sender][id];
 
+        // close the books
+        if(idData.amount > 0) {
+            if(idData.lastUpdateTimestamp > block.timestamp) {
+                //uint256 currentTimestamp = block.timestamp > endTime ? endTime : block.timestamp;
+                uint256 timeDelta = block.timestamp - idData.lastUpdateTimestamp;
+                
+                idData.timeWeighted += timeDelta * idData.amount;
+            }
+        }
+        
+        if(userData.amount > 0){
+            if(userData.lastUpdateTimestamp > block.timestamp){
+
+                uint256 timeDelta = block.timestamp - userData.lastUpdateTimestamp;
+                userData.timeWeighted += timeDelta * userData.amount;
+            }
+        }
+
+        // book new amount
+        idData.amount += amount;
+        userData.amount += amount;
+
+        emit Staked(msg.sender, id, amount);
+ 
+        // grab MOCA
+        MOCA_TOKEN.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function unstake(bytes32 id, uint256 amount) external {}
+    function unstake(bytes32 id, uint256 amount) external {
+        require(id != bytes32(0), "Invalid id");
 
-    //function unstakeAll() external {}
+        // check if id exists
+        // note: how?
+
+        // cache
+        Data memory idData = ids[msg.sender];
+        Data memory userData = users[msg.sender][id];
+
+        // sanity checks
+        require(userData.amount >= amount, "Insufficient user balance");
+        require(idData.amount >= amount, "Insufficient id balance");
+
+        // close the books
+        if(idData.amount > 0) {
+            if(idData.lastUpdateTimestamp > block.timestamp) {
+                //uint256 currentTimestamp = block.timestamp > endTime ? endTime : block.timestamp;
+                uint256 timeDelta = block.timestamp - idData.lastUpdateTimestamp;
+                
+                idData.timeWeighted += timeDelta * idData.amount;
+            }
+        }
+        
+        if(userData.amount > 0){
+            if(userData.lastUpdateTimestamp > block.timestamp){
+
+                uint256 timeDelta = block.timestamp - userData.lastUpdateTimestamp;
+                userData.timeWeighted += timeDelta * userData.amount;
+            }
+        }
+
+        // book outflow
+        idData.amount -= amount;
+        userData.amount -= amount;
+
+        emit Unstaked(msg.sender, id, amount);
+
+        // transfer moca
+        MOCA_TOKEN.safeTransfer(msg.sender, amount);
+    }
+
     
-
 }
