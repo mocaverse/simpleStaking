face4432c93db11149604a2934a8ab894e35be48
calnix <calnix.289@gmail.com>
2024-05-28 11:10:36 +0800

    updated pragma

--- src/SimpleStaking.sol
+++ src/SimpleStaking.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 
-//import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract SimpleStaking {
