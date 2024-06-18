74e3e09786210348a87a0f65f4b23d6f5bc61f5d
Tony Fung <tonyfung99@gmail.com>
2024-06-18 18:19:36 +0800

    update the script with more log info, large samlple for testing

--- script/BatchDeposit.s.js
+++ script/BatchDeposit.s.js
@@ -29,41 +29,62 @@ async function run() {
   const stakingAddress = process.env.STAKING_CONTRACT;
   const tokenAddress = process.env.TOKEN_CONTRACT;
 
-  const SimpleStaking = await hre.ethers.getContractFactory("SimpleStaking");
-  const simpleStaking = await SimpleStaking.attach(stakingAddress);
+  const signer = new hre.ethers.Wallet(deployerPrivateKey, hre.ethers.provider);
 
-  const token = await hre.ethers.getContractAt("IERC20", tokenAddress);
+  const simpleStaking = await hre.ethers.getContractAt("SimpleStaking", stakingAddress, signer);
+  const token = await hre.ethers.getContractAt("IERC20", tokenAddress, signer);
 
   // load csv from file path
   const csvPath = process.env.CSV_PATH;
   const userRecords = await loadCSV(csvPath);
 
-  const signer = new hre.ethers.Wallet(deployerPrivateKey, hre.ethers.provider);
+  const executeTime = Date.now();
 
-  console.log("Script started by: ", await signer.getAddress(), "Balance: ", hre.ethers.formatEther(await hre.ethers.provider.getBalance(signer)));
+  console.log(
+    "Script started by: ",
+    await signer.getAddress(),
+    "ETH: ",
+    hre.ethers.formatEther(await hre.ethers.provider.getBalance(signer)),
+    "token: ",
+    hre.ethers.formatEther(await token.balanceOf(await signer.getAddress()))
+  );
 
   await token.approve(stakingAddress, await token.balanceOf(await signer.getAddress()));
 
-  // get the first 10 records and stake them
-  const BATCH_SIZE = 1;
+  const BATCH_SIZE = 300;
   for (let i = 0; i < userRecords.length; i += BATCH_SIZE) {
     const batch = userRecords.slice(i, i + BATCH_SIZE);
     const addresses = batch.map((record) => record.address);
     const amounts = batch.map((record) => record.amount).map((amount) => (REQUIRE_PARSE ? hre.ethers.parseEther(amount) : amount));
 
-    console.log(`Staking ${addresses}`);
+    const writeContent = addresses.map((address, index) => `${address},${amounts[index]}`).join("\n");
+
+    console.log(`Staking from ${i} to ${i + BATCH_SIZE} of ${userRecords.length}`);
     try {
+      const gasPrice = (await hre.ethers.provider.getFeeData()).gasPrice;
+
+      const estimatedGas = await simpleStaking.stakeBehalf.estimateGas(addresses, amounts);
+      console.log(
+        `Current gas price per unit: ${gasPrice} txn gas unit: ${estimatedGas} total cost: ${hre.ethers.formatEther(
+          gasPrice * estimatedGas
+        )} token: ${hre.ethers.formatEther(await token.balanceOf(await signer.getAddress()))}`
+      );
       // currently seems it's callable for owner only
       const txn = await simpleStaking.stakeBehalf(addresses, amounts);
       await txn.wait();
-      fs.appendFileSync("stake_result.log", `${addresses},${amounts}, success\n`);
+      console.log(`Success, txn hash: ${txn.hash}`);
+      fs.appendFileSync(`stake_success_${executeTime}.log`, writeContent + "\n");
     } catch (error) {
       console.warn(error);
-      fs.appendFileSync("stake_result.log", `${addresses},${amounts}, failed: ${error}\n`);
+      fs.appendFileSync(`stake_error_${executeTime}.log`, writeContent + "\n");
     }
   }
 
-  console.log("Script finished, Balance: ", hre.ethers.formatEther(await hre.ethers.provider.getBalance(signer)));
+  console.log(
+    `Script finished, ETH:  ${hre.ethers.formatEther(await hre.ethers.provider.getBalance(signer))} token: ${await token.balanceOf(
+      await signer.getAddress()
+    )}`
+  );
 }
 
 run().catch((error) => {
